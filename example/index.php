cef6f1a29b8af7320db878c74b3e6dfcdbb006c1
Doka <kadokwr@gmail.com>
2019-03-30 09:37:03 -0300

    Initial commit

---
+++ example/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <?php
+    require __DIR__ . "/../vendor/autoload.php";
+
+    $op = new \KadokWeb\Optimizer\Optimizer();
+    echo $op
+        ->optimize(
+            "Optimizer Happy and @KadokWeb",
+            "Is a compact and easy-to-use tag creator to optimize your site",
+            "https://www.kadokweb.com.br/KadokWeb/optimizer/example/",
+            "https://www.kadokweb.com.br/uploads/images/2017/11/curso-de-html5-preparando-ambiente-de-trabalho-aula-02-1511276983.jpg"
+        )
+        ->publisher(
+            "kadokweb",
+            "kadokweb",
+            "1117886656730375220464",
+            "117886656730375220464"
+        )
+        ->twitterCard(
+            "@kadokweb",
+            "@kadokweb",
+            "kadokweb.com.br",
+            "summary_large_image"
+        )
+        ->openGraph(
+            "kadokweb",
+            "pt_BR",
+            "article"
+        )
+        ->facebook("1859747784058591")//->facebook(null, [9283729732123, 912837192372, 73642734723])
+        ->render();
+    ?>
+</head>
+<body>
+
+<h1><?= $op->title; ?></h1>
+<p><?= $op->description; ?></p>
+
+<?php
+echo "<pre>", print_r($op->data(), true), "</pre>";
+echo "<pre>", print_r(array_map("htmlspecialchars", $op->debug()), true), "</pre>";
+?>
+
+</body>
+</html>
